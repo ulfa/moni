8b47ec5b3a84e77369244f3f87204ef649853bd0
Ulf <uaforum1@googlemail.com>
2013-02-26 13:49:26 +0100

    integration of searcher

--- src/loggerl_app.erl
+++ src/loggerl_app.erl
@@ -10,6 +10,7 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
+	application:start(searcher),
     loggerl_sup:start_link().
 
 stop(_State) ->
