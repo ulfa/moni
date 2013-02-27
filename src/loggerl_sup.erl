eef980df280dc2c9fc41c22b53f2d113cdd1a4aa
Ulf <uaforum1@googlemail.com>
2013-02-27 20:48:07 +0100

    moved node_repo to searcher

--- src/loggerl_sup.erl
+++ src/loggerl_sup.erl
@@ -24,5 +24,5 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
-    {ok, { {one_for_one, 5, 10}, [?CHILD(node_repo ,worker)]} }.
+    {ok, { {one_for_one, 5, 10}, [?CHILD(worker)]} }.
 
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mkdir -p log/sasl
-erl -name loggerl@localhost -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s loggerl -s erlbuild
+erl -sname loggerl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s loggerl -s erlbuild
