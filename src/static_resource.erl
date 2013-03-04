f980823f17bdbc2b17f493538f850807492f8557
Ulf <uaforum1@googlemail.com>
2013-03-04 09:15:48 +0100

    refactored the name of the app

--- src/static_resource.erl
+++ src/static_resource.erl
@@ -12,7 +12,7 @@
 -record(context, {docroot,fullpath,fileinfo}).
  
 init(DocRoot) -> 
-	Directory = filename:join(code:priv_dir(loggerl),DocRoot),
+	Directory = filename:join(code:priv_dir(moni),DocRoot),
 	{ok, #context{docroot=Directory}}.
  
 resource_exists(ReqData, Context) ->
