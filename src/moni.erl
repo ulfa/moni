e6a8bb4f8bd0c682eeaed88ec9a1704f50a1e549
Ulf <uaforum1@googlemail.com>
2013-03-04 09:16:15 +0100

    refactored the name of the module

--- src/moni.erl
+++ src/moni.erl
@@ -35,7 +35,7 @@ start() ->
 	ensure_started(inets), 
 	ensure_started(crypto),
 	ensure_started(mochiweb),
-	application:set_env(webmachine, webmachine_logger_module, webmachine_logger),
+	%%application:set_env(webmachine, webmachine_logger_module, webmachine_logger),
 	ensure_started(webmachine),
 	ensure_started(searcher),	
 	application:start(?MODULE).
