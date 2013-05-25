03ae9fdd98e983dce4c2d07d47943c7703140279
Ulf <uaforum1@googlemail.com>
2013-05-25 06:31:07 +0200

    changed structure to rebar release structure

---
+++ apps/moni/src/moni_sup.erl
@@ -0,0 +1,42 @@
+
+-module(moni_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+%% Helper macro for declaring children of supervisor
+-define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+
+%% ===================================================================
+%% API functions
+%% ===================================================================
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+%% ===================================================================
+%% Supervisor callbacks
+%% ===================================================================
+
+%% @spec init([]) -> SupervisorTree
+%% @doc supervisor callback.
+init([]) ->
+    Ip = case os:getenv("WEBMACHINE_IP") of false -> "0.0.0.0"; Any -> Any end,
+    {ok, Dispatch} = file:consult(filename:join(
+                         [filename:dirname(code:which(?MODULE)),
+                          "..", "priv", "dispatch.conf"])),
+    WebConfig = [
+                 {ip, Ip},
+                 {port, 8000},
+                 {log_dir, "priv/log"},
+                 {dispatch, Dispatch}],
+    Web = {webmachine_mochiweb, {webmachine_mochiweb, start, [WebConfig]}, permanent, 5000, worker, dynamic},
+    Processes = [Web],
+    {ok, { {one_for_one, 10, 10}, Processes} }.
+
+
